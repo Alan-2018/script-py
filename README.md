b33cabe541773aeb6d76039890384b40fb735e1f
“flower” <flower.cong@basebit.ai>
2023-03-19 02:09:54 +0800

    [test]

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # script-py
+
+## vscode
+### pylint
---
+++ test.py
@@ -0,0 +1,16 @@
+# pylint: disable=pointless-string-statement
+'''
+'''
+
+'''
+uuid hex & string
+"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+'''
+
+
+
+if __name__ == "__main__":
+    print('hello world')
+
+
